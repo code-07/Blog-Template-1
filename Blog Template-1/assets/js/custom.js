a1af4ada585f92195ff7bcc50c7de3a605ceec04
Abu Taher <abutaherku10@gmail.com>
2017-11-01 02:25:50 +0600

    Blog Template

---
+++ Blog Template-1/assets/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	    // Portfolio Type Style
+     $(".typed-from-js").typed({
+        strings: ["Abu Taher", "Web Designer", "Web Developer"],
+        cursorChar: " ",
+        typeSpeed: 100,
+        loop: true,
+        loopCount: true,
+    });
+
+});
